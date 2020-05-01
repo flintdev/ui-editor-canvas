dabb274d7c8b2d0f3487424ffae69d155c21a8b8
cc189 <cc189dev@gmail.com>
2020-05-01 14:16:06 -0700

    add plugin.js webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     context: __dirname,
@@ -42,6 +43,9 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./example/index.html",
             filename: "./index.html"
-        })
+        }),
+        new CopyPlugin([
+            { from: `./resources/material-widgets/plugin.js`, to: `./` },
+        ]),
     ]
 };
