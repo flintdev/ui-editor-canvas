7aa204478b9112c0223fa385d3125dd166df29ea
cc189 <cc189dev@gmail.com>
2020-05-02 21:10:43 -0700

    add WidgetDndWrapper example

--- example/ExampleContainer.tsx
+++ example/ExampleContainer.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { withStyles, createStyles } from '@material-ui/core/styles';
 import UIEditorCanvas from "../src/UIEditorCanvas";
 import { Operations, EditorLib, ComponentData, Components } from "../src/UIEditorCanvas/interface";
+import { WidgetDndWrapper } from "../dist";
 import { WidgetName, WidgetProps } from "@flintdev/material-widgets";
 import { Button, TextField } from '@material-ui/core';
 import { initComponentsData, customNodes } from "./data/initComponentsData";
@@ -99,58 +100,40 @@ class ExampleContainer extends React.Component<any, object> {
                 <div className={classes.left}>
                     {/* isDnd Switch */}
                     <FormControlLabel
-                        control={ <Switch checked={isDnd} onChange={() => this.handleChange()} value="checkedA" /> }
+                        control={<Switch checked={isDnd} onChange={() => this.handleChange()} value="checkedA" />}
                         label="isDnd"
                     />
 
                     {/* Add Widgets Menu */}
-                    <SimpleMenu operations={this.operations}/>
+                    <SimpleMenu operations={this.operations} />
 
                     {/* Drag to create */}
-                    <div style={{opacity: isButtonSelected? 0 : 1, maxHeight: 40, minHeight: 40}} onDragEnd={() => this.handleSelectButton(false)} onMouseUp={() => this.handleSelectButton(false)}>
-                        {
-                            !isButtonSelected ? <Button onMouseDown={() => this.handleSelectButton(true)}>Drag Me To Create Button</Button> :
-                                getWidget("KubeEditor", {
-                                    params: {
-                                        data: {},
-                                        customNodes: customNodes,
-                                        hidePanel: "show",
-                                        hideMinimap: "show"
-                                    },
-                                    dnd: true,
-                                    draggableProps: {
-                                        draggableId: 'created-by-drag',
-                                        index: 0
-                                    },
-                                    onDragEnd: (result: any) => {
-                                        if (this.operations.onDragEnd) {
-                                            count += 1;
-                                            result.dragToCreate = true;
-                                            result.dragComponentData = {
-                                                id: `button-dragged-${count}`,
-                                                name: "KubeEditor",
-                                                params: {
-                                                    data: {},
-                                                    customNodes: customNodes,
-                                                    hidePanel: "view",
-                                                    hideMinimap: "show"
-                                                },
-                                                children: [],
-                                                path: [],
-                                                tag: ""
-                                            };
-                                            this.operations.onDragEnd(result)
-                                        }
-                                        this.handleSelectButton(false)
-                                    }
-                                } as any)
+                    <WidgetDndWrapper
+                        operations={this.operations}
+                        getWidget={getWidget}
+                        width={800}
+                        height={30}
+                        component={<Button>Drag Wrapper</Button>}
+                        widgetData={
+                            {
+                                id: `dragged-widget-uuid`,
+                                name: "KoniEditor",
+                                params: {
+                                    data: {},
+                                    customNodes: [],
+                                    hidePanel: "view",
+                                    hideMinimap: "show"
+                                },
+                                children: [],
+                                path: [],
+                                tag: ""
+                            }
                         }
-                    </div>
-
-                    <Button onClick={() => {if (this.operations.selectComponentById) this.operations.selectComponentById("Button-1")}}>Select "Button-1"</Button>
-                    <Button onClick={() => {if (this.operations.selectComponentById) this.operations.selectComponentById("Grid-2")}}>Select "Grid-2"</Button>
-                    <Button onClick={() => {if (this.operations.selectComponentById) this.operations.selectComponentById("Label-3")}}>Select "Label-3"</Button>
-                    <Button onClick={() => {if (this.operations.deleteComponentById) this.operations.deleteComponentById("Button-2")}}>Delete "Button-2"</Button>
+                    />
+                    <Button onClick={() => { if (this.operations.selectComponentById) this.operations.selectComponentById("Button-1") }}>Select "Button-1"</Button>
+                    <Button onClick={() => { if (this.operations.selectComponentById) this.operations.selectComponentById("Grid-2") }}>Select "Grid-2"</Button>
+                    <Button onClick={() => { if (this.operations.selectComponentById) this.operations.selectComponentById("Label-3") }}>Select "Label-3"</Button>
+                    <Button onClick={() => { if (this.operations.deleteComponentById) this.operations.deleteComponentById("Button-2") }}>Delete "Button-2"</Button>
                 </div>
 
                 {/* CENTER */}
@@ -159,9 +142,9 @@ class ExampleContainer extends React.Component<any, object> {
                         isDnd={isDnd}
                         operations={this.operations}
                         components={components}
-                        editorLib={{getWidget: getWidget}}
+                        editorLib={{ getWidget: getWidget }}
                         componentsUpdated={(components: Components) => {
-                            this.setState({components: components})
+                            this.setState({ components: components })
                             console.log('>>> componentsUpdated.components', components)
                         }}
                         componentOnSelect={(componentData: ComponentData) => {
